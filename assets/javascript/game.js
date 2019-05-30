9e29fbbb3967a3789399d4c40b1ce101794f6233
mfxod <46651745+mfxod@users.noreply.github.com>
2019-05-29 22:13:49 -0400

    added variables and functions, psuedocode notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,70 @@
-// --- STEPS ---
-// use keyboard event to check if userGuess is in wordChoice
-// if it is replace the correct _ (or _s) with the letter
-// then guessCounter-- and lettersLeft--
+// --- USER GUESS LOOP ---
+// document.onkeyup = function(event) {
+//     if (userGuess is in wordArr) {
+//         replace the corresponding _(s) in blankArr with userGuess
+//         maybe use forEach()?
+//         then guessCounter--
+//     } else {
+//         add userGuess to wrongGuesses
+//         no duplicates in wrongGuesses
+//         then guessCounter--
+//     }
+// }
 
-// if userGuess isn't in wordChoice add it alreadyGuessedText
-// then guessCounter--
+//  --- SOME OF THE CONDITIONS ---
+// if (user guesses all letters in word) {
+//     alert("Congrats");
+//     reset();
+// }
 
-// don't repeat letters in alreadyGuessedText - ???
-
-// if user guesses all the letters, alert("congrats") and reset() 
-// if guessCounter = 0 and user hasn't guessed all letters, reset()
-// if user guesses all ten words alert("You Win!")
-
+// if (guessCounter = 0 and user hasn't guessed word) {
+//     show word;
+//     reset();
+// }
 
 // --- QUESTIONS ---
-// where and how to use arrays?
-// .split("") then .join("")?
-// convert userGuess with .toLowerCase()?
-// is there a way to store a key event differently than below?
-
+// .split(), .join(), .charAt(), match() with regex, concat()? 
+// convert userGuess and wordBank words with .toLowerCase()?
 
 var wins = 0;
-var winsText = document.getElementById("wins");
-
-var currentWordText = document.getElementById("current-word");
-
-var guessCounter = 12;
-var guessCounterText = document.getElementById("guesses-left");
-
-var alreadyGuessed = []
-var alreadyGuessedText = document.getElementById("already-guessed");
-
 var wordBank = ["John", "Paul", "George", "Ringo"];
-var wordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
+var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+var wordArr = word.split("");
+var blankArr = [];
+var userGuess = "";
+var guessCounter = 12;
+var wrongGuesses = [];
+var lettersLeft = word.length;
 
-var wordArr = [];
-for (var i = 0; i < wordChoice.length; i++) {
-    wordArr[i] = "_";
-    currentWordText.textContent = wordArr.join(" ");
-}
+var winsText = document.getElementById("wins");
+var wordText = document.getElementById("word");
+var guessCounterText = document.getElementById("guess-counter");
+var wrongGuessesText = document.getElementById("wrong-guesses");
 
 winsText.textContent = wins;
+guessCounterText.textContent = guessCounter;
+wrongGuessesText.textContent = wrongGuesses.join(" ");
 
-var lettersLeft = wordChoice.length;
+function showBlankArr() {
+    for (var i = 0; i < word.length; i++) {
+        blankArr[i] = "_";
+        wordText.textContent = blankArr.join(" ");
+    }
+}
+
+function guessLoop() {
+    for (var i = 0; i < word.length; i++) {
+        if (wordArr[i] === userGuess) {
+            // replace the blankArr[i] with userGuess;
+        } else {
+            // add userGuess to wrongGuessesText only once
+            wrongGuesses.push(userGuess);
+        }
+    }
+}
 
 document.onkeyup = function(event) {
-    var userGuess = event.key;
-    console.log(userGuess);
+    userGuess = event.key;
+    guessLoop();
     guessCounter--;
-    guessCounterText.textContent = guessCounter;
 }
